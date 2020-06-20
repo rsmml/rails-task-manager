0fa2f384b4324178f73444d5925b5fc3aa952ace
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-06-20 12:44:57 +0200

    new task manager created, with link to go back and link to go to /new

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,7 +14,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
-    redirect_to task_path(@taks)
+    redirect_to task_path(@task)
   end
 
   private
