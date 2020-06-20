51ec7143a0ed760307e4077bb6d366d4b8b8dc7f
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-06-20 12:53:37 +0200

    edit task done

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,18 @@ Rails.application.routes.draw do
   # CRUD ACTIONS
   # Read ALL my Tasks
   get 'tasks', to: 'tasks#index'
+
   # Create a New Task
   get 'tasks/new', to: 'tasks#new', as: :new_task
   post 'tasks', to: 'tasks#create'
+
+  # Update a Task
+  get 'taks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
   # Read One Task
   get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # Delete a Task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
